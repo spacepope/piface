10a0613594e4b17e5fadb253d38f54e7c6f278f8
Hannes Petersen <jhpetersen@gmx.de>
2017-11-06 22:13:28 +0100

    added interrupt handling and open without init methods.

--- pfio.cc
+++ pfio.cc
@@ -10,7 +10,17 @@ void PfioInit(const v8::FunctionCallbackInfo<v8::Value>& args) {
 	if (args.Length() >= 1) {
 		hw_addr = Integer::New(isolate, args[0]->IntegerValue())->Value();
 	}
-	uint8_t pifacedigital_fd = pifacedigital_open(hw_addr);
+	int pifacedigital_fd = pifacedigital_open(hw_addr);
+	args.GetReturnValue().Set(pifacedigital_fd);
+}
+
+void PfioOpenNoInit(const v8::FunctionCallbackInfo<v8::Value>& args) {
+	Isolate* isolate = args.GetIsolate();
+	uint8_t hw_addr = 0;
+	if (args.Length() >= 1) {
+		hw_addr = Integer::New(isolate, args[0]->IntegerValue())->Value();
+	}
+	int pifacedigital_fd = pifacedigital_open_noinit(hw_addr);
 	args.GetReturnValue().Set(pifacedigital_fd);
 }
 
@@ -26,7 +36,7 @@ void PfioDeinit(const v8::FunctionCallbackInfo<v8::Value>& args) {
 void PfioDigitalRead(const v8::FunctionCallbackInfo<v8::Value>& args) {
 	Isolate* isolate = args.GetIsolate();
 	uint8_t pin = Integer::New(isolate, args[0]->IntegerValue())->Value();
-	uint8_t val = pifacedigital_digital_read(pin);
+	uint8_t val = pifacedigital_digital_read(pin) ^ 1;
 	args.GetReturnValue().Set(val);
 }
 
@@ -43,7 +53,7 @@ void PfioReadInput(const v8::FunctionCallbackInfo<v8::Value>& args) {
 	if (args.Length() >= 1) {
 		hw_addr = Integer::New(isolate, args[0]->IntegerValue())->Value();
 	}
-	uint8_t val = pifacedigital_read_reg(INPUT, hw_addr);
+	uint8_t val = pifacedigital_read_reg(INPUT, hw_addr) ^ 0xFF;
 	args.GetReturnValue().Set(val);
 }
 
@@ -67,14 +77,48 @@ void PfioWriteOutput(const v8::FunctionCallbackInfo<v8::Value>& args) {
 	pifacedigital_write_reg(val, OUTPUT, hw_addr);
 }
 
+void PfioEnableInterrupts(const v8::FunctionCallbackInfo<v8::Value>& args) {
+	int res = pifacedigital_enable_interrupts();
+	bool ok = (res == 0);
+	args.GetReturnValue().Set(ok);
+}
+
+void PfioDisableInterrupts(const v8::FunctionCallbackInfo<v8::Value>& args) {
+	int res = pifacedigital_disable_interrupts();
+	bool ok = (res == 0);
+	args.GetReturnValue().Set(ok);
+}
+
+void PfioWaitForInput(const v8::FunctionCallbackInfo<v8::Value>& args) {
+	Isolate* isolate = args.GetIsolate();
+	int timeout = Integer::New(isolate, args[0]->IntegerValue())->Value();
+	uint8_t hw_addr = 0;
+	if (args.Length() > 1) {
+		hw_addr = Integer::New(isolate, args[1]->IntegerValue())->Value();
+	}
+	uint8_t inputs;
+	int res = pifacedigital_wait_for_input(&inputs, timeout, hw_addr);
+	if (res < 0) {
+		isolate->ThrowException(String::NewFromUtf8(isolate, "Error waiting on interrupt."));
+	} else if (res == 0) {
+		isolate->ThrowException(String::NewFromUtf8(isolate, "Interrupt timed out."));
+	} else {
+		args.GetReturnValue().Set(inputs);
+	}
+}
+
 void init(Handle<Object> exports) {
 	NODE_SET_METHOD(exports, "init", PfioInit);
+	NODE_SET_METHOD(exports, "open_noinit", PfioOpenNoInit);
 	NODE_SET_METHOD(exports, "deinit", PfioDeinit);
 	NODE_SET_METHOD(exports, "digital_read", PfioDigitalRead);
 	NODE_SET_METHOD(exports, "digital_write", PfioDigitalWrite);
 	NODE_SET_METHOD(exports, "read_input", PfioReadInput);
 	NODE_SET_METHOD(exports, "read_output", PfioReadOutput);
 	NODE_SET_METHOD(exports, "write_output", PfioWriteOutput);
+	NODE_SET_METHOD(exports, "enable_interrupts", PfioEnableInterrupts);
+	NODE_SET_METHOD(exports, "disable_interrupts", PfioDisableInterrupts);
+	NODE_SET_METHOD(exports, "wait_for_input", PfioWaitForInput);
 }
 
 NODE_MODULE(pfio, init);
